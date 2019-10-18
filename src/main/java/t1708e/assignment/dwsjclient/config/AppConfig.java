058b77dfe45d6d16177078a0273df77b445bf98c
SlowV <quocviet.hn98@gmail.com>
2019-10-18 15:38:16 +0700

    hoan thanh dang ky dang nhap

---
+++ src/main/java/t1708e/assignment/dwsjclient/config/AppConfig.java
@@ -0,0 +1,50 @@
+package t1708e.assignment.dwsjclient.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import t1708e.assignment.dwsjclient.service.comment.CommentService;
+import t1708e.assignment.dwsjclient.service.comment.CommentServiceServiceLocator;
+import t1708e.assignment.dwsjclient.service.image.ImageService;
+import t1708e.assignment.dwsjclient.service.image.ImageServiceServiceLocator;
+import t1708e.assignment.dwsjclient.service.place.PlaceService;
+import t1708e.assignment.dwsjclient.service.place.PlaceServiceServiceLocator;
+import t1708e.assignment.dwsjclient.service.rating.RatingService;
+import t1708e.assignment.dwsjclient.service.rating.RatingServiceServiceLocator;
+import t1708e.assignment.dwsjclient.service.user.UserService;
+import t1708e.assignment.dwsjclient.service.user.UserServiceServiceLocator;
+
+import javax.xml.rpc.ServiceException;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public UserService userService() throws ServiceException {
+        UserServiceServiceLocator userServiceServiceLocator = new UserServiceServiceLocator();
+        return userServiceServiceLocator.getUserServicePort();
+    }
+
+    @Bean
+    public ImageService imageService() throws ServiceException {
+        ImageServiceServiceLocator imageServiceServiceLocator = new ImageServiceServiceLocator();
+        return imageServiceServiceLocator.getImageServicePort();
+    }
+
+    @Bean
+    public CommentService commentService() throws ServiceException {
+        CommentServiceServiceLocator commentServiceServiceLocator = new CommentServiceServiceLocator();
+        return commentServiceServiceLocator.getCommentServicePort();
+    }
+
+    @Bean
+    public RatingService ratingService() throws ServiceException {
+        RatingServiceServiceLocator ratingtServiceServiceLocator = new RatingServiceServiceLocator();
+        return ratingtServiceServiceLocator.getRatingServicePort();
+    }
+
+    @Bean
+    public PlaceService placeService() throws ServiceException {
+        PlaceServiceServiceLocator placetServiceServiceLocator = new PlaceServiceServiceLocator();
+        return placetServiceServiceLocator.getPlaceServicePort();
+    }
+
+}
---
+++ src/main/java/t1708e/assignment/dwsjclient/service/image/Comment.java
@@ -0,0 +1,391 @@
+/**
+ * Comment.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package t1708e.assignment.dwsjclient.service.image;
+
+public class Comment  implements java.io.Serializable {
+    private java.lang.String content;
+
+    private long createdAt;
+
+    private long deletedAt;
+
+    private long id;
+
+    private t1708e.assignment.dwsjclient.service.image.Image image;
+
+    private t1708e.assignment.dwsjclient.service.image.Place place;
+
+    private int status;
+
+    private long updatedAt;
+
+    private t1708e.assignment.dwsjclient.service.image.User user;
+
+    public Comment() {
+    }
+
+    public Comment(
+           java.lang.String content,
+           long createdAt,
+           long deletedAt,
+           long id,
+           t1708e.assignment.dwsjclient.service.image.Image image,
+           t1708e.assignment.dwsjclient.service.image.Place place,
+           int status,
+           long updatedAt,
+           t1708e.assignment.dwsjclient.service.image.User user) {
+           this.content = content;
+           this.createdAt = createdAt;
+           this.deletedAt = deletedAt;
+           this.id = id;
+           this.image = image;
+           this.place = place;
+           this.status = status;
+           this.updatedAt = updatedAt;
+           this.user = user;
+    }
+
+
+    /**
+     * Gets the content value for this Comment.
+     * 
+     * @return content
+     */
+    public java.lang.String getContent() {
+        return content;
+    }
+
+
+    /**
+     * Sets the content value for this Comment.
+     * 
+     * @param content
+     */
+    public void setContent(java.lang.String content) {
+        this.content = content;
+    }
+
+
+    /**
+     * Gets the createdAt value for this Comment.
+     * 
+     * @return createdAt
+     */
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+
+    /**
+     * Sets the createdAt value for this Comment.
+     * 
+     * @param createdAt
+     */
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+
+    /**
+     * Gets the deletedAt value for this Comment.
+     * 
+     * @return deletedAt
+     */
+    public long getDeletedAt() {
+        return deletedAt;
+    }
+
+
+    /**
+     * Sets the deletedAt value for this Comment.
+     * 
+     * @param deletedAt
+     */
+    public void setDeletedAt(long deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+
+    /**
+     * Gets the id value for this Comment.
+     * 
+     * @return id
+     */
+    public long getId() {
+        return id;
+    }
+
+
+    /**
+     * Sets the id value for this Comment.
+     * 
+     * @param id
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    /**
+     * Gets the image value for this Comment.
+     * 
+     * @return image
+     */
+    public t1708e.assignment.dwsjclient.service.image.Image getImage() {
+        return image;
+    }
+
+
+    /**
+     * Sets the image value for this Comment.
+     * 
+     * @param image
+     */
+    public void setImage(t1708e.assignment.dwsjclient.service.image.Image image) {
+        this.image = image;
+    }
+
+
+    /**
+     * Gets the place value for this Comment.
+     * 
+     * @return place
+     */
+    public t1708e.assignment.dwsjclient.service.image.Place getPlace() {
+        return place;
+    }
+
+
+    /**
+     * Sets the place value for this Comment.
+     * 
+     * @param place
+     */
+    public void setPlace(t1708e.assignment.dwsjclient.service.image.Place place) {
+        this.place = place;
+    }
+
+
+    /**
+     * Gets the status value for this Comment.
+     * 
+     * @return status
+     */
+    public int getStatus() {
+        return status;
+    }
+
+
+    /**
+     * Sets the status value for this Comment.
+     * 
+     * @param status
+     */
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+
+    /**
+     * Gets the updatedAt value for this Comment.
+     * 
+     * @return updatedAt
+     */
+    public long getUpdatedAt() {
+        return updatedAt;
+    }
+
+
+    /**
+     * Sets the updatedAt value for this Comment.
+     * 
+     * @param updatedAt
+     */
+    public void setUpdatedAt(long updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+
+    /**
+     * Gets the user value for this Comment.
+     * 
+     * @return user
+     */
+    public t1708e.assignment.dwsjclient.service.image.User getUser() {
+        return user;
+    }
+
+
+    /**
+     * Sets the user value for this Comment.
+     * 
+     * @param user
+     */
+    public void setUser(t1708e.assignment.dwsjclient.service.image.User user) {
+        this.user = user;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof Comment)) return false;
+        Comment other = (Comment) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            ((this.content==null && other.getContent()==null) || 
+             (this.content!=null &&
+              this.content.equals(other.getContent()))) &&
+            this.createdAt == other.getCreatedAt() &&
+            this.deletedAt == other.getDeletedAt() &&
+            this.id == other.getId() &&
+            ((this.image==null && other.getImage()==null) || 
+             (this.image!=null &&
+              this.image.equals(other.getImage()))) &&
+            ((this.place==null && other.getPlace()==null) || 
+             (this.place!=null &&
+              this.place.equals(other.getPlace()))) &&
+            this.status == other.getStatus() &&
+            this.updatedAt == other.getUpdatedAt() &&
+            ((this.user==null && other.getUser()==null) || 
+             (this.user!=null &&
+              this.user.equals(other.getUser())));
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        if (getContent() != null) {
+            _hashCode += getContent().hashCode();
+        }
+        _hashCode += new Long(getCreatedAt()).hashCode();
+        _hashCode += new Long(getDeletedAt()).hashCode();
+        _hashCode += new Long(getId()).hashCode();
+        if (getImage() != null) {
+            _hashCode += getImage().hashCode();
+        }
+        if (getPlace() != null) {
+            _hashCode += getPlace().hashCode();
+        }
+        _hashCode += getStatus();
+        _hashCode += new Long(getUpdatedAt()).hashCode();
+        if (getUser() != null) {
+            _hashCode += getUser().hashCode();
+        }
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(Comment.class, true);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("http://service.travel.t1708e/", "comment"));
+        org.apache.axis.description.ElementDesc elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("content");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "content"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("createdAt");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "createdAt"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "long"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("deletedAt");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "deletedAt"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "long"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("id");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "id"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "long"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("image");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "image"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://service.travel.t1708e/", "image"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("place");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "place"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://service.travel.t1708e/", "place"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("status");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "status"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "int"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("updatedAt");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "updatedAt"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "long"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("user");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "user"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://service.travel.t1708e/", "user"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+    }
+
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+    /**
+     * Get Custom Serializer
+     */
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanSerializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+    /**
+     * Get Custom Deserializer
+     */
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanDeserializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+}
