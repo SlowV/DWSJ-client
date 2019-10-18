5dc854cc1cc18be62db7f847893cbb5d6aeafbaa
SlowV <quocviet.hn98@gmail.com>
2019-10-18 20:53:56 +0700

    thêm security cho user và admin

--- src/main/java/t1708e/assignment/dwsjclient/config/WebSecurityConfig.java
+++ src/main/java/t1708e/assignment/dwsjclient/config/WebSecurityConfig.java
@@ -1,49 +1,79 @@
 package t1708e.assignment.dwsjclient.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import t1708e.assignment.dwsjclient.service.user.User;
+import t1708e.assignment.dwsjclient.service.user.UserService;
+import t1708e.assignment.dwsjclient.util.StringConst;
+
+import java.rmi.RemoteException;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    UserService userService;
+
     @Bean
     public UserDetailsService userDetailsService() {
-        // ensure the passwords are encoded properly
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(users.username("user").password("password").roles("USER").build());
-        manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
-        return manager;
+       return username -> {
+           try {
+               User user = userService.findByUsername(username);
+               if (user == null){
+                   throw new UsernameNotFoundException("User not found");
+               }
+               return org.springframework.security.core.userdetails.User.builder()
+                       .username(user.getUsername())
+                       .password(user.getPassword())
+                       .roles(user.getRole())
+                       .build();
+           } catch (RemoteException e) {
+               e.printStackTrace();
+           }
+           return null;
+       };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService());
+        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/users/register").permitAll()
-//                .and()
-//                .formLogin()
-//                .loginPage("/users/login")
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .permitAll();
-        // trien khai chuc nang cho nhanh!
-        http.authorizeRequests().anyRequest().permitAll();
+        http
+                .authorizeRequests()
+                .antMatchers("/users/register").permitAll()
+                .antMatchers("/admin/**").hasAnyRole(StringConst.ROLE_ADMIN)
+                .and()
+                .formLogin()
+                .loginPage("/users/login")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/users/logout")
+                .logoutSuccessUrl("/")
+                .permitAll();
+
+
+//        // trien khai chuc nang cho nhanh!
+//        http.authorizeRequests().anyRequest().permitAll();
     }
 }
